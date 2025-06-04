ff52585d73a53b7f9f6f223bf9701d12ee5133a8
youjeong Jeong <wjddbwjd1205@naver.com>
2025-06-04 12:59:40 +0900

    fix: 불필요한 fetch 요청을 보내고 있는 useEffect 삭제

--- src/features/ProductList/hooks/useCart.ts
+++ src/features/ProductList/hooks/useCart.ts
@@ -1,14 +1,6 @@
-import { useEffect } from 'react';
-
 import { useCartRequest } from './useCartRequest';
 export const useCart = () => {
-
-  const { refetch, addToCart, increaseQuantity, decreaseQuantity, deleteFromCart } =
-    useCartRequest();
-
-  useEffect(() => {
-    refetch();
-  }, [refetch]);
+  const { addToCart, increaseQuantity, decreaseQuantity, deleteFromCart } = useCartRequest();
 
   return {
     addToCart,
